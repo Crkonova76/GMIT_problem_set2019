aa3c3c413048305e113aff6832a86c0b1f32d5c6
Crkonova76 <47481671+Crkonova76@users.noreply.github.com>
2019-03-03 22:00:20 +0000

    Create problem_set_2.py

---
+++ problem_set_2.py
@@ -0,0 +1,18 @@
+#Martina Crkonova, 2019-02-28
+#Problem Set 2019, Programming and Scripting, GMIT
+#Program that outputs whether or not today is a day that begins with the letter T
+
+"""
+
+
+
+
+"""
+
+import datetime
+
+
+if datetime.datetime.today().weekday() == 1 or 3:
+    print("Yes-today begins with T.")
+else:
+    print("No-Today does not begin with a T.")
