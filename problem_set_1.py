2f0bffe6ed4b3dd789e7e73896ad42c34312a854
Crkonova76 <47481671+Crkonova76@users.noreply.github.com>
2019-03-03 21:59:09 +0000

    Create problem_set_1.py

---
+++ problem_set_1.py
@@ -0,0 +1,32 @@
+# # Martina Crkonova, 2019-02-28
+# Problem Set 2019, Python sumupto, Programming and Scripting, GMIT
+# Program calculating sum of all numbers between given number and one
+
+"""
+
+
+
+
+"""
+#i = int(input ( "Enter the positive integer: "))
+
+# total = 0
+
+# while i > 0:
+#     total = total + i
+#     i = i - 1
+
+# print (total)
+
+a = int(input ( "Enter the positive integer: "))
+
+total = 0
+result = 0
+
+while total < a:
+
+    total = total + 1
+    result = total + result
+    
+      
+print(result)
